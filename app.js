e5b01f1f5a19b46db632b4d1eb48d643186edab2
nacerillo <nicholascerillo@gmail.com>
2021-01-12 14:25:20 -0800

    Guessing Game start

---
+++ app.js
@@ -0,0 +1,36 @@
+var name = prompt("Hi! What's your name?");
+
+
+alert("Nice to meet you " + name + "! and welcome to my webpage!");
+
+var crazy = prompt("Being from Florida, I must be totally crazy, right?");
+if( crazy.toLowerCase() == "y" || crazy.toLowerCase() == "yes"){
+    alert("Nope! I have my quirks, sure, but I'm not a complete looney, lol.");
+}
+else { 
+    alert("Thats right. I'm a completely normal person... relatively...");
+}
+
+var weather = prompt("I can't stand the weather here in Washington!");
+if(weather.toLowerCase() == "y" || weather.toLowerCase() == "yes"){
+    alert("This is mostly true. While i dont mind the cold, the winters/falls here in washington do make me long for the sunshine again.")
+}
+else {
+    alert("I'm getting used to it, but to be honest I'm not a fan lol");
+}
+
+var moons = prompt("Jupiter has over a dozen moons");
+    if(moons.toLowerCaser() == "y" || moons.toLowerCaser() == "yes"){
+        alert("That's right! but did you know that Jupitor has OVER 70 OF THEM! Thats a LOT of friggin moons!");
+}
+    else {
+        alert("Jupitor has well over a dozen moons. In fact, Jupitor has over 70 moons!");
+    }
+
+var year =  prompt("Will things be ANY better in 2021?");
+    if(year.toLowerCase() == "y" || year.toLowerCase() == "yes"){
+        alert("Damn right it will! We're starting out the new year with learning and personal growth! We're gonna MAKE this year better!");
+    }
+    else {
+        alert("Things WILL get better, we just gotta believe that we can MAKE it better");
+    }
